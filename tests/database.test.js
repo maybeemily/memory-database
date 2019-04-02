1e343fc2031203cdcba12a3e962048a7ac99f262
emily baier <emilyrbaier@gmail.com>
2019-04-02 15:55:19 -0700

    pass first test to return empty object

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,3 +1,9 @@
 const { MemoryDatabase } = require('../lib/database.js');
 
 
+describe('messing with memory database', () => {
+    it('create new database and get empty object', () => {
+        const newDatabase = new MemoryDatabase();
+        expect(newDatabase.store).toEqual({});
+    });
+});
