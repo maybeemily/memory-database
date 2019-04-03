519d20771747af03304200a3e4d89615145e5981
emily baier <emilyrbaier@gmail.com>
2019-04-03 11:48:31 -0700

    removed dead code, fixed spacing

--- tests/database.test.js
+++ tests/database.test.js
@@ -6,18 +6,22 @@ describe('messing with memory database', () => {
     beforeEach(() => {
         newDatabase = new MemoryDatabase();
     });
+
     it('create new database and get empty object', () => {
         expect(newDatabase.store).toEqual({});
     });
+
     it('capture uuid and set as key ', () => {
         const user = newDatabase.create({ username: 'my name', email: 'my@email.com' });
         expect(user._id).toEqual(expect.any(String));
     });
+
     it('findById function returns id', () => {
         const user = newDatabase.create({ username: 'my name', email: 'my@email.com' });
         expect(newDatabase.findById(user._id)).toEqual(expect.any(Object));
         expect((newDatabase.findById(user._id))._id).toEqual(user._id);
     });
+
     it('returns objects in the store', () => {
         const user = newDatabase.create({ username: 'my name', email: 'my@email.com' });
         const user2 = newDatabase.create({ username: 'my name', email: 'my@email.com' });
@@ -27,24 +31,29 @@ describe('messing with memory database', () => {
         expect(newDatabase.find()).toContain(user2);
         expect(newDatabase.find()).toContain(user3);
     });
+
     it('findByIdAndUpdate', () => {
         const user = newDatabase.create({ username: 'my name', email: 'my@email.com' });
         const updatedUser = newDatabase.findByIdAndUpdate(user._id, { username: 'spongebob', email: 'spongebob@email.com' });
         expect(updatedUser.username).toEqual('spongebob');
     });
+
     it('findByIdAndUpdate returns null if ID doesnt exist', () => {
         const updatedUser = newDatabase.findByIdAndUpdate(123456, { username: 'spongebob', email: 'spongebob@email.com' });
         expect(updatedUser).toEqual(null);
     });
+
     it('findByIdAndDelete', () => {
         const user = newDatabase.create({ username: 'my name', email: 'my@email.com' });
         const deletedUser = newDatabase.findByIdAndDelete(user._id);
         expect(deletedUser).toEqual(user);
     });
+
     it('findByIdAndDelete returns null if ID doesnt exist', () => {
         const deletedUser = newDatabase.findByIdAndDelete(123456);
         expect(deletedUser).toEqual(null);
     });
+    
     it('deletes all properties/replaces this.store with empty object', () => {
         const emptyStore = newDatabase.drop();
         expect(emptyStore).toEqual({});
